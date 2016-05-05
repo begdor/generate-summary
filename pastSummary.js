61921af65beb4ef3a9931a73b0acdb33ffafe14c
begdor <dulikunhust@gmail.com>
2016-05-05 10:56:31 -0700

    correct the status in the history report

--- pastSummary.js
+++ pastSummary.js
@@ -37,40 +37,65 @@ exports.pastSummary = function(items,date){
   //generate a standard date starting of the day
   var currentDate = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
   var list = [];
+  //MARK: store the bids data into two List
+  var openList = [];
+  var exeList = [];
   var res = [];
   for (let i in items) {
-    if (items[i].status === 'open' ||
-    ((items[i].status === 'executed'|| items[i].status === 'executedArchived')
+    if (items[i].status === 'open') {
+      openList.push(items[i]);
+    }
+    else if (((items[i].status === 'executed'|| items[i].status === 'executedArchived')
     && items[i].originalPrice != items[i].strikePrice)) {
-      list.push(items[i]);
+      exeList.push(items[i]);
     }
   }
 
-  sortDate(list, 'createdDate', 0, list.length - 1);
-  //sortDate(exeList, 'executionDate', 0, exeList.length - 1);
-  //console.log(openList);
-  var first = new Date(list[0].createdDate);
+  sortDate(openList, 'createdDate', 0, openList.length - 1);
+  sortDate(exeList, 'executionDate', 0, exeList.length - 1);
+
+  var first = new Date(openList[0].createdDate);
+  var second = new Date(exeList[0].createdDate);
+  first = first>second?second:first;
   //var firstExe = new Date(exeList[0].executionDate);
   var startDate = new Date(first.getUTCFullYear(),
                            first.getUTCMonth(),
                            first.getUTCDate());
-  var i = 1;
+  var i = 1, j = 0;
   while (startDate <= currentDate) {
-    while(i < list.length) {
+    var tempOpen = [];
+    var tempExe = [];
+    //MARK: find the open bids before the startDate
+    while(i < openList.length) {
       //find the index of list that createDate is later than startDate
-      if (new Date(list[i].createdDate) <= startDate ) {
+      if (new Date(openList[i].createdDate) <= startDate ) {
         i++;
       }
-      //generate the summary of that day and added it to res list, increase the day by one
       else {
-        var temp = Summary.calSummary(list.slice(0,i-1));
-        temp['date'] = new Date(startDate);
-        res.push(temp);
-        startDate.setDate(startDate.getDate() + 1);
+        break;
       }
     }
-    //if the index reach the end of the list but the day is not currentDate
-    temp = Summary.calSummary(list.slice(0,i-1));
+    while(j < exeList.length) {
+      //find the index of list that createDate is later than startDate, change the status when necessary
+      if (new Date(exeList[j].createDate) > startDate ) {
+        break;
+      }
+      else if (new Date(exeList[j].executionDate) > startDate ) {
+        tempOpen.push(Object.assign({},exeList[j])) ;
+        tempOpen[tempOpen.length-1].status = 'open';
+        j++;
+      }
+      else {
+        tempExe.push(exeList[j]);
+        j++;
+      }
+    }
+    j = 0;
+    //generate the summary of that day and added it to res list, increase the day by one
+    var tempList = openList.slice(0,i-1);
+    tempList = tempList.concat(tempOpen);
+    tempList = tempList.concat(tempExe);
+    var temp = Summary.calSummary(tempList);
     temp['date'] = new Date(startDate);
     res.push(temp);
     startDate.setDate(startDate.getDate() + 1);
